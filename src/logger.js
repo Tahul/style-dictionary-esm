d3616d8468767c74cbc9dd30615d67b19b7f286b
Yaël Guilloux <yael.guilloux@gmail.com>
2022-09-02 14:48:33 +0200

    fix(logger): create logger for scope

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,4 @@
 import consola from 'consola'
-const _logger = consola.withScope('style-dictionary')
+const _logger = consola.create().withScope('style-dictionary')
 export const logger = () => _logger
 export default logger
