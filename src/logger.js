b92b8cce046feeabc6958e21503dc9836bf739af
Yaël Guilloux <yael.guilloux@gmail.com>
2022-09-08 02:15:22 +0200

    fix(logger): fix logger

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,9 @@
 import consola from 'consola'
-const _logger = consola.create().withScope('style-dictionary')
-export const logger = () => _logger
+let _logger
+export const logger = () => {
+  if (!_logger)
+    _logger = consola.create().withDefaults().withScope('style-dictionary')
+
+  return _logger
+}
 export default logger
