1e2be99a3a6503e8c704e28733352bbebdc7ae60
Yaël Guilloux <yael.guilloux@gmail.com>
2023-12-04 01:04:53 +0100

    feat(silent): support `silent` on platform to avoid console.log

--- fs.js
+++ fs.js
@@ -1,5 +1,5 @@
 // Allow to be overridden by setter, set default to memfs for browser env, node:fs for node env
-export let fs = (await import('@bundled-es-modules/memfs')).default;
+export let fs = undefined;
 
 // since ES modules exports are read-only, use a setter
 export const setFs = (_fs) => {
